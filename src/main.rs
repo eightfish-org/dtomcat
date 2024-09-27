38c2ecff6bb3299d55408b5e33d00631c324a5b0
Daogang Tang <daogangtang@gmail.com>
2024-09-26 19:09:53 -0700

    update to the coordination with latest vintage.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ use std::thread;
 
 const CHANNEL_VIN2WORKER: &str = "vin2worker";
 
+// dtomcat action
+pub(crate) const ACTION_NEW_BLOCK_HEIGHT: &str = "block_height";
+pub(crate) const ACTION_UPLOAD_WASM: &str = "upload_wasm";
+pub(crate) const ACTION_UPGRADE_WASM: &str = "upgrade_wasm";
+
 // #[derive(Debug, Serialize, Deserialize)]
 // enum EventType {
 //     UploadWasmFile,
@@ -63,32 +68,44 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn process_message(msg: InputOutputObject) -> Result<(), Box<dyn Error>> {
     println!("Processing message:");
 
-    let info: Info = serde_json::from_str(&msg.model)?;
-
     match &msg.action[..] {
-        "upload_wasmfile" => {
-            let body = &msg.data;
-            // check the digest of body
-            // if msg.digest != sha256_check(body) {}
+        ACTION_NEW_BLOCK_HEIGHT => {
+            let body: [u8; 8] = msg.data.try_into().unwrap_or([0; 8]);
+            // convert to u64
+            let block_height = u64::from_be_bytes(body);
 
-            // generate a new wasm binary file
-            let path = format!("proto_wasm_files/{}-v{}.wasm", info.proto, info.version);
-            // Write the result to a new file
-            let mut output_file = fs::File::create(path)?;
-            output_file.write_all(body)?;
+            // do something
+            log::info!("Block height: {block_height}");
         }
-        "do_upgrade" => {
+        ACTION_UPLOAD_WASM => {
+            let wasm_hash = hex::encode(msg.data);
+            let wasm_binary = msg.ext;
+            // check the digest of body
+            // if wasm_hash != sha256_check(wasm_binary) {}
+
+            // let proto = msg.proto;
+            // let version = msg.model;
+            // generate a new wasm binary file
+            let path = format!("wasm_files/{wasm_hash}.wasm");
+            // Write the result to a new file
+            let mut output_file = fs::File::create(&path)?;
+            output_file.write_all(&wasm_binary)?;
+
+            log::info!("on upload wasm, wasm file {path} saved.");
+        }
+        ACTION_UPGRADE_WASM => {
             // TODO: check whether the existence of the corresponding wasm file
             // if doesn't exist, return early
 
             // Read the template file
             let template = fs::read_to_string("spin_tmpl.txt")?;
 
-            // this id will be received from the redis
-            let proto_id = &info.proto;
-            let proto_version = &info.version;
+            // let info: Info = serde_json::from_str(&msg.model)?;
+            let wasm_hash = hex::encode(msg.data);
+            let proto = msg.proto;
+            let version = wasm_hash;
             // Define the replacements
-            let replacements = [("$proto_id", proto_id), ("$proto_version", proto_version)];
+            let replacements = [("$proto_id", &proto), ("$proto_version", &version)];
 
             // Perform the replacements
             let mut result = template;
@@ -97,34 +114,36 @@ fn process_message(msg: InputOutputObject) -> Result<(), Box<dyn Error>> {
             }
 
             // generate a new intermediate spin config file
-            let path = format!(
-                "proto_config_files/spin-{}-v{}.toml",
-                proto_id, proto_version
-            );
+            let path = format!("tmp_configs/{}-{}.toml", proto, version);
             // Write the result to a new file
             let mut output_file = fs::File::create(&path)?;
             output_file.write_all(result.as_bytes())?;
 
-            println!("Replacement complete. Check output file.");
+            log::info!("Replacement complete. Check output file.");
 
+            // Important:  when start this dtomcat process, we must specify these two envs
             let redis_host = std::env::var("REDIS_HOST")?;
             let db_host = std::env::var("DB_HOST")?;
 
             // spawn new spin instance
             let mut env_vars = HashMap::new();
             env_vars.insert("SPIN_VARIABLE_REDIS_HOST".to_string(), redis_host.clone());
-            env_vars.insert("SPIN_VARIABLE_PROTO_ID".to_string(), msg.proto.clone());
+            // env_vars.insert("SPIN_VARIABLE_PROTO_ID".to_string(), proto.clone());
 
-            let redis_env = "REDIS_URL_ENV='redis://localhost:6379'";
+            // let redis_env = "REDIS_URL_ENV='redis://localhost:6379'";
+            let redis_env = format!("REDIS_URL_ENV='{redis_host}'");
             let db_env = format!(
                 "DB_URL_ENV='host={} user=postgres password=postgres dbname={} sslmode=disable'",
-                db_host, msg.proto
+                db_host, proto
             );
 
             let _proto_handle = run_command_with_env(
                 "spin",
                 &["up", "-f", &path, "-e", &redis_env, "-e", &db_env],
                 env_vars,
+            );
+            log::info!(
+                "on proto upgrade, the protocol {proto} has been upgraded to version: {version}."
             );
             // Don't join.
             // match proto_handle.join().expect("Thread panicked") {
@@ -136,7 +155,7 @@ fn process_message(msg: InputOutputObject) -> Result<(), Box<dyn Error>> {
             // }
         }
         _ => {
-            eprintln!("error action type in this msg from redis.")
+            log::error!("error action type in this msg from redis.");
         }
     }
 
